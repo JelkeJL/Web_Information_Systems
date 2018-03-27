dc025604a651f18bdad25e397fb7df038da35ecc
JelkeJL <jelkejoosen@hotmail.com>
2018-03-27 14:29:13 +0200

    Adding a map to the page

--- functions.js
+++ functions.js
@@ -1,1 +1,30 @@
 // this is the javascript hi jelke
+
+// $(document).ready(function(){
+//     function initMap() {
+//         var ArenbergIII = {lat: 50.863002, lng: 4.678974};
+//         var map = new google.maps.Map(document.getElementById('map'), {
+//             zoom: 4,
+//             center: ArenbergIII
+//         });
+//         var marker = new google.maps.Marker({
+//             position: ArenbergIII,
+//             map: map
+//         });
+//     }
+//     async defer
+//     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZHYymzYflI1c7mbhFTcsb6NN4ss7LBwU&callback=initMap"
+// });
+
+function initMap() {
+    var ArenbergIII = {lat: 50.863002, lng: 4.678974};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 9,
+        center: ArenbergIII
+    });
+    var marker = new google.maps.Marker({
+        position: ArenbergIII,
+        map: map
+    });
+}
+
