ec9061581e05bd937397b5eed7f6df0eea531525
JelkeJL <jelkejoosen@hotmail.com>
2018-04-17 14:11:55 +0200

    Remove current location attempt

--- functions.js
+++ functions.js
@@ -16,8 +16,6 @@
 //     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZHYymzYflI1c7mbhFTcsb6NN4ss7LBwU&callback=initMap"
 // });
 
-
-var map, infoWindow;
 function initMap() {
     var ArenbergIII = {lat: 50.863002, lng: 4.678974};
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -29,33 +27,3 @@ function initMap() {
         map: map
     });
 }
-        infoWindow = new google.maps.InfoWindow;
-
-
-if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
