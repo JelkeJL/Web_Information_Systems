e7dcc297137745e92798e77f386b40e1a96b7f73
JelkeJL <jelkejoosen@hotmail.com>
2018-04-24 11:56:15 +0200

    Change info window content + add location image

--- functions.js
+++ functions.js
@@ -47,7 +47,7 @@ var map, infoWindow;
             };
 
             infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
+            infoWindow.setContent('You are here.');
             infoWindow.open(map);
             map.setCenter(pos);
           }, function() {
