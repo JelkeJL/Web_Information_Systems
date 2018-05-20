7eba1e95cb391e213727d72cd888b44952305b9d
JelkeJL <jelkejoosen@hotmail.com>
2018-05-20 12:43:08 +0200

    seperate xml retrieving functions
    make sure different variable names are used for fakbar vs. cudi

--- functions.js
+++ functions.js
@@ -105,80 +105,36 @@ var map, infoWindow;
       };
 
 
-
-
-
-
-//http://jsfiddle.net/VLQKw/1/ used as a reference for conditional dropdown menus
-
-$(document).ready(function() {
-
-  $('#group').bind('change', function (e) { 
-    //console.log("change");
-    $('#overview').hide()
-    if ($('#group').val() == 'empty'){
-      $('#faculty_biomedical').hide();
-      
-      $('#faculties_humanities').hide();
-      $('#faculties_science').hide();
-
-      $('#degrees_arts').hide();
-    }
-    else if($('#group').val() == 'humanities') {
-      
-      $('#faculties_science').hide();
-      $('#faculty_biomedical').hide();
-      $('#faculties_humanities').show();
-      $('#fac_hum').bind('change', function (e){
-        if ($('#fac_hum').val() != 'arts'){
-          $('#degrees_arts').hide();
-          if ($('#fac_hum').val() == 'economics'){
-            $('#overview').show();
-          }
-        }
-        else if ($('#fac_hum').val() == 'arts'){
-          $('#degrees_arts').show();
-
-        }
-      }).trigger('change');
-    }
-    else if( $('#group').val() == 'science') {
-      $('#faculties_science').show();
-      
-      $('#faculties_humanities').hide();
-      $('#faculty_biomedical').hide();
-
-      $('#degrees_arts').hide()
-    }
-    else if( $('#group').val() == 'biomedical') {
-      $('#faculty_biomedical').show();
-      
-      $('#faculties_humanities').hide();
-      $('#faculties_science').hide();
-
-      $('#degrees_arts').hide();
-    }
-
-  }).trigger('change');
-
-});
-
-
-
-
+//reference to fakbar_new.xml
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         myFunction(this);
     };
+
 };
+
 xhttp.open("GET", "fakbar_new.xml", true);
 xhttp.send();
 
+//reference to cudi.xml
+var xhttp_ = new XMLHttpRequest();
+xhttp_.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myFunction_(this);
+    };
+};
+xhttp_.open("GET", "cudi.xml", true);
+xhttp_.send();
+
+
+//function to retrieve fakbar
 function myFunction(xml) {
   // console.log(xml.responseText);
     var xmlDoc = xml.responseXML;
     
+    var fakbarname = xmlDoc.getElementsByTagName("name")[0].childNodes[0].nodeValue;
+
     document.getElementById("fakbar_from_xml").innerHTML = 
     xmlDoc.getElementsByTagName("name")[0].childNodes[0].nodeValue + ": " 
     + xmlDoc.getElementsByTagName("description")[0].childNodes[0].nodeValue;
@@ -189,22 +145,14 @@ function myFunction(xml) {
     //console.log(coordinates);
     ecofaklat = coordinates[0]
     ecofaklng = coordinates[1]
-    console.log(ecofaklat,ecofaklng)
+    console.log(fakbarname, ecofaklat,ecofaklng)
 
     eco = [ecofaklat, ecofaklng]
    
    //$('#overview').show()
 };
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        myFunction_(this);
-    };
-};
-xhttp.open("GET", "cudi.xml", true);
-xhttp.send();
-
+//function to retrieve cudi
 function myFunction_(xml) {
   // console.log(xml.responseText);
     var xmlDoc = xml.responseXML;
@@ -225,4 +173,70 @@ function myFunction_(xml) {
    
    //$('#overview').show()
 };
+
+
+//http://jsfiddle.net/VLQKw/1/ used as a reference for conditional dropdown menus
+
+$(document).ready(function() {
+
+  $('#group').bind('change', function (e) { 
+    //console.log("change");
+
+    $('#overview').hide()
+
+    if ($('#group').val() == 'empty'){
+      $('#faculty_biomedical').hide();      
+      $('#faculties_humanities').hide();
+      $('#faculties_science').hide();
+      $('#degrees_arts').hide();
+    }
+
+    else if($('#group').val() == 'humanities') {     
+      $('#faculties_science').hide();
+      $('#faculty_biomedical').hide();
+
+      $('#faculties_humanities').show();
+
+      $('#fac_hum').bind('change', function (e){
+        if ($('#fac_hum').val() != 'arts'){
+          $('#degrees_arts').hide();
+
+          if ($('#fac_hum').val() == 'economics'){
+            $('#overview').show();
+          };
+
+        }
+
+        else if ($('#fac_hum').val() == 'arts'){
+          $('#degrees_arts').show();
+        };
+
+      }).trigger('change');
+    }
+
+    else if( $('#group').val() == 'science') {
+      $('#faculties_science').show();
+      
+      $('#faculties_humanities').hide();
+      $('#faculty_biomedical').hide();
+      $('#degrees_arts').hide()
+    }
+
+    else if( $('#group').val() == 'biomedical') {
+      $('#faculty_biomedical').show();
+      
+      $('#faculties_humanities').hide();
+      $('#faculties_science').hide();
+
+      $('#degrees_arts').hide();
+    }
+
+  }).trigger('change');
+
+});
+
+
+
+
+
 
